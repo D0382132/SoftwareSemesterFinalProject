e83a69d57cac740c772f5bcb81356b6679aa0d10
D0382132 <tm7tm7tm7tm7@yahoo.com.tw>
2017-05-13 16:54:43 +0800

    建立讀檔mathod

--- SoftwareSemesterFinalProject/src/memberAccountData.java
+++ SoftwareSemesterFinalProject/src/memberAccountData.java
@@ -1,15 +1,56 @@
 
+import java.io.*;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.lang.Character;
 public class memberAccountData 
 
 {
 	private String account ;
 	private String password;
+	private int point;
 	
-	public memberAccountData(String inputAccount , String inputPassword )
+	/*public boolean creatAccount(String inputAccount , String inputPassword )
 	{
-		account = new String(inputAccount);
-		password = new String(inputPassword);
+		this.account = new String(inputAccount);
+		this.password = new String(inputPassword);
+		this.point = 0;
 		
+	}*/
+	
+	private boolean readFile(String inputAccount)
+	{
+		boolean returnBoolean = false;
+		try
+		{
+			FileReader fr=new FileReader("memberData.txt");
+			BufferedReader br=new BufferedReader(fr);
+			String line;
+			 
+		      while (( line = br.readLine()) != null) 
+		      {
+		    	  String[] subLine = line.split("/");
+		          if(compareAccount(subLine[0]))
+		          {
+		        	 this.account = subLine[0];
+		        	 this.password = subLine[1];
+		        	 char temp = subLine[2].charAt(0);
+		        	 this.point = Character.getNumericValue(temp);
+		        	 returnBoolean = true;
+		          }
+		      }
+		}
+		catch(IOException e)
+		{
+			System.out.println(e);
+		}
+		
+		if(account == null)
+		{
+			returnBoolean = false;	
+		}
+		
+		return returnBoolean;
 	}
 	
 	public boolean compareAccount (String inputAccount)
@@ -22,7 +63,6 @@ public class memberAccountData
 		
 		return returnValue;
 	}
-	
 	public boolean comparePassword (String inputPassword)
 	{
 		boolean returnValue;
@@ -32,6 +72,14 @@ public class memberAccountData
 			returnValue = false;
 		
 		return returnValue;
+	}
+	public int getPoint()
+	{
+		return point;
+	}
+	public void addPoint() 
+	{
+		this.point = this.point + 1;
 	}
 
 }
